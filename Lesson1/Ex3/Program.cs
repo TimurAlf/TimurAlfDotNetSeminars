3197583302d4530f578b029354f09ceac14311dc
Timur <47ta@bk.ru>
2022-08-08 13:53:02 +0500

    Ex3

---
+++ Lesson1/Ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x==1)
+{
+Console.WriteLine("Sun");
+}
+if(x==2)
+{
+Console.WriteLine("Mon");
+}
+if(x==3)
+{
+Console.WriteLine("Tue!");
+}
+if(x==4)
+{
+Console.WriteLine("Wed");
+}
+if(x==5)
+{
+Console.WriteLine("Thu");
+}
+if(x==6)
+{
+Console.WriteLine("Fri");
+}
+if(x==7)
+{
+Console.WriteLine("Sat");
+}
