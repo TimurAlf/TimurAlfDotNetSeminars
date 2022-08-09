b2e2dec1b1e01f045cd5d944ee5ed115bcf66f8e
Timur <47ta@bk.ru>
2022-08-09 16:57:28 +0500

    Ex2

---
+++ Homework2/Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+void Show()
+{
+    int show = new Random().Next(1, 10000);
+    int c = 99;
+    int a = show / 10;
+    int b = a % 10;
+    if (show > c)
+    {
+        Console.WriteLine(show);
+        Console.Write(b);
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет ");
+    }
+}
+Show();
+
+
+
+
