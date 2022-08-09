4f2f76ee2bc02785f41e5c2636339022e604d081
Timur <47ta@bk.ru>
2022-08-09 15:24:31 +0500

    Ex1

---
+++ Lesson2/Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+void Ex1()
+{
+int val = new Random().Next(10, 100);
+int a = val / 10;
+int b = val % 10;
+Console.WriteLine(val);
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+}
+Ex1();
