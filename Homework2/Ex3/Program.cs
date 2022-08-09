b59e090f428a5ead6d9c11a5be75da7ca1f18ec4
Timur <47ta@bk.ru>
2022-08-09 17:04:20 +0500

    Ex3

---
+++ Homework2/Ex3/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+void Weekend()
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x == 1)
+    {
+        Console.WriteLine("Выходной");
+    }
+    if (x == 2)
+    {
+        Console.WriteLine("Нет");
+    }
+    if (x == 3)
+    {
+        Console.WriteLine("Нет!");
+    }
+    if (x == 4)
+    {
+        Console.WriteLine("Нет");
+    }
+    if (x == 5)
+    {
+        Console.WriteLine("Нет");
+    }
+    if (x == 6)
+    {
+        Console.WriteLine("Нет");
+    }
+    if (x == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    if (x > 7)
+    {
+        Console.WriteLine("Дней недели всего 7 :)");
+    }
+}
+Weekend();
