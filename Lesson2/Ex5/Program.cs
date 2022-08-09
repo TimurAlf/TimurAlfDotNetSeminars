6cde0bc83769012146bd35e4bbf5676fd8bbc2cf
Timur <47ta@bk.ru>
2022-08-09 16:24:46 +0500

    Ex5.1

--- Lesson2/Ex5/Program.cs
+++ Lesson2/Ex5/Program.cs
@@ -4,21 +4,24 @@
 //-4,16 да
 //25,5 да
 //8,9 нет
-
-int a = 4, b = 8;
-if (a * a == b)
+void Sqr()
 {
-    Console.WriteLine("yes");
-}
-else
-{
-    if (b * b == a)
+    int a = 4, b = 8;
+    if (a * a == b)
     {
         Console.WriteLine("yes");
     }
     else
     {
-        Console.WriteLine("no");
+        if (b * b == a)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
     }
 }
 
+Sqr();
