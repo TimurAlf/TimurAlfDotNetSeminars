4de5b094f04031494f411df1b0bc8c5f1b969041
Timur <47ta@bk.ru>
2022-08-09 16:26:03 +0500

    Ex2.1

--- Lesson2/Ex2/Program.cs
+++ Lesson2/Ex2/Program.cs
@@ -3,11 +3,15 @@
 //456 -> 46
 //782-> 72
 //918 -> 98
-
+void Del()
+{
 int val = new Random().Next(100, 1000);
 int a = val/100;
 int b = val%10;
 
 Console.WriteLine(val);
 Console.Write(a);
-Console.Write(b);
+Console.Write(b);   
+}
+Del();
+
