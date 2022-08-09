bf01924483fe713beb14f3ddcb0f90902fa954fc
Timur <47ta@bk.ru>
2022-08-09 15:37:25 +0500

    Ex2

---
+++ Lesson2/Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет 2 цифру этого числа.
+//456 -> 46
+//782-> 72
+//918 -> 98
+
+int val = new Random().Next(100, 1000);
+int a = val/100;
+int b = val%10;
+
+Console.WriteLine(val);
+Console.Write(a);
+Console.Write(b);
