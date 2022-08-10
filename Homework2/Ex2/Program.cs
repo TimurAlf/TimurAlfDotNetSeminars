5ca6de8fe52fe2de84450d43a6a7b5dac4ff1726
Timur <47ta@bk.ru>
2022-08-10 16:38:49 +0500

    Ex2.3

--- Homework2/Ex2/Program.cs
+++ Homework2/Ex2/Program.cs
@@ -2,12 +2,12 @@
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
-*/
+
 void Show()
 {
-    int show = new Random().Next(1, 10000);
+    int show = new Random().Next(1,10000);
     int c = 99;
-    int a = show / 10;
+    int a = show / 100;
     int b = a % 10;
     if (show > c)
     {
@@ -20,7 +20,16 @@ void Show()
     }
 }
 Show();
-
-
+*/
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
 
 
