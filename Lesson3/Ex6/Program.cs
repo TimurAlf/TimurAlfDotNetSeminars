e3409c507523a8180bbd952ab0f653f60d0fbc03
Timur <47ta@bk.ru>
2022-08-16 15:20:01 +0500

    New3

--- Lesson3/Ex6/Program.cs
+++ Lesson3/Ex6/Program.cs
@@ -5,6 +5,15 @@
 
 int x1 = new Random().Next(-10, 10), x2 = new Random().Next(1, 5);
 int y1 = new Random().Next(-10, 10), y2 = new Random().Next(1, 5);
+
+/*либо использовать кортеж для заполнения координат
+(int x, int y) a, b;
+a = (1, 2);
+b = (3, 4);
+System.Console.WriteLine(a.x);
+System.Console.WriteLine(b.y);
+*/
+
 System.Console.WriteLine("Координата точки A("+x1+","+y1+")");
 System.Console.WriteLine("Координата точки B("+x2+","+y2+")");
 System.Console.Write("Расстояние между точками А и В в 2D пространстве равно: ");
