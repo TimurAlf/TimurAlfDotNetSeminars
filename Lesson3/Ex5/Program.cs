df839bdd7434de38989e5d81d012c2527310453a
Timur <47ta@bk.ru>
2022-08-16 14:22:43 +0500

    New

---
+++ Lesson3/Ex5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int coord = new Random().Next(1, 4);
+System.Console.WriteLine("Четверть: "+ coord);
+System.Console.Write("Диапозон возможных координат: ");
+if (coord == 1) System.Console.Write("x > 0 и y > 0");
+if (coord == 2) System.Console.Write("x < 0 и y > 0");
+if (coord == 3) System.Console.Write("x < 0 и y < 0");
+if (coord == 4) System.Console.Write("x > 0 и y < 0");
+
