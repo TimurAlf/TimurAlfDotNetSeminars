9fd943c2cb969f999a13d66160dc4dab84edc286
Timur <47ta@bk.ru>
2022-08-16 10:12:53 +0500

    New

---
+++ Lesson3/Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Как ограничить пользователя, чтобы он ввел только число от -10 до 10.
+//Как можно упростить? использовать int.TryParse
+
+int GetTryParse(string v)
+{
+    int a = -20;
+    while (true)
+    {
+        Console.Write(v);
+        string s = Console.ReadLine();
+        bool f = int.TryParse(s, out a); // оператор пытается превратить строку в тип который мы указали
+        if (f && a >= -10 && a <= 10)
+        return a;
+    }   
+}
+int a = GetTryParse("Введите только число от -10 до 10:  ");
