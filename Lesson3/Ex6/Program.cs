b0b387cdc456bc30c14ca08bfa37ead6c61b3b1b
Timur <47ta@bk.ru>
2022-08-16 14:49:27 +0500

    New

---
+++ Lesson3/Ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+// d= ((x2-x1)^2 + (y2-y1)^2)^1\2
+
+int x1 = new Random().Next(-10, 10), x2 = new Random().Next(1, 5);
+int y1 = new Random().Next(-10, 10), y2 = new Random().Next(1, 5);
+System.Console.WriteLine("Координата точки A("+x1+","+y1+")");
+System.Console.WriteLine("Координата точки B("+x2+","+y2+")");
+System.Console.Write("Расстояние между точками А и В в 2D пространстве равно: ");
+double result = Math.Pow(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)), 0.5);
+System.Console.Write(result);
