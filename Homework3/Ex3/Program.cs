dd62ee5a4bff9c2e883d466aa185cbe4cb1b77b6
Timur <47ta@bk.ru>
2022-08-16 22:36:03 +0500

    Initial commit

---
+++ Homework3/Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int N = new Random().Next(1, 10);
+System.Console.WriteLine(N);
+
+for (int i = 1; i <= N; i++)
+{
+    int result = i*i*i;
+    System.Console.WriteLine(result);
+}
