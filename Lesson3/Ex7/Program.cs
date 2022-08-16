aea8c0fe7e85cc0e427143f6dc99f8a9f06c3296
Timur <47ta@bk.ru>
2022-08-16 15:00:39 +0500

    New

---
+++ Lesson3/Ex7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,
+
+int N = new Random().Next(1, 10);
+System.Console.WriteLine(N);
+
+for (int i = 1; i <= N; i++)
+{
+    int result = i*i;
+    System.Console.WriteLine(result);
+}
