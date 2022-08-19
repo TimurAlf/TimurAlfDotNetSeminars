0f84984bd12618a13ff0f94492536523383c034f
Timur <47ta@bk.ru>
2022-08-19 20:32:06 +0500

    Initial

---
+++ Lesson4/Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Сумма цифр от 1 до A.
+
+int NewMethod(int arg)
+{
+    int sum = 0;
+    for (int i = 0; i <= arg; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int a = 10;
+int sum = NewMethod(a);
+
+System.Console.WriteLine($"sum: {sum}"); // интерполяция строк, делается для того чтобы не смешивались типы данных
