2759f393eb697bdb53fdf3c9a496cd392b627ca7
Timur <47ta@bk.ru>
2022-08-20 19:45:33 +0500

    Initial

---
+++ Intern/Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Почувствуй себя интерном*
+//  0. Вывести квадрат числа
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+NewMethod(num);
+
+void NewMethod(int num)
+{
+    int result = num * num;
+    System.Console.WriteLine($"{num} * {num} = {result}");
+}
