54e4be8e53d4422d189bbcbe498d6cd095cb270c
Timur <47ta@bk.ru>
2022-08-20 12:29:05 +0500

    New

--- Lesson1/Ex1/Program.cs
+++ Lesson1/Ex1/Program.cs
@@ -3,7 +3,12 @@
 // например -3 = 9
 // например -7 = 49
 
-int value = 10;
-int result = value * value;
-Console.WriteLine($"result: {result}");
+int num = new Random().Next(-10, 10);
+System.Console.Write(num + " * " + num +" = ");
+int result = NewMethod(num);
+Console.Write($"{result}");
 
+int NewMethod(int num)
+{
+    return num * num;
+}
