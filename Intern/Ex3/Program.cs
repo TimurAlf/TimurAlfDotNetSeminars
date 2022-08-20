a9cd6e75757d2acae525b97f6171e551302704b7
Timur <47ta@bk.ru>
2022-08-20 19:56:00 +0500

    Initial

---
+++ Intern/Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Сomparison(a, b);
+
+static void Сomparison(int a, int b)
+{
+    if (a > b)
+    {
+        System.Console.WriteLine($"{a} > {b}");
+    }
+    else
+    {
+        System.Console.WriteLine($"{a} < {b}");
+    }
+}
