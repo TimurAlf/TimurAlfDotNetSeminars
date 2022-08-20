9f5af380793e895f068e065a2c5fff844b7a3e17
Timur <47ta@bk.ru>
2022-08-20 23:05:10 +0500

    New

---
+++ Intern/Ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8. Показать четные числа от 1 до N
+
+System.Console.Write("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+Show(x);
+
+void Show(int x)
+{
+    for (int i = 1; i <= x; i++)
+    {
+        Console.Write($"{i,3}");
+    }
+}
