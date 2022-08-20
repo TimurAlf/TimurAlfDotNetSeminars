c1a50410b6604a2ab36be92ceb76103b034ba637
Timur <47ta@bk.ru>
2022-08-20 22:54:55 +0500

    New

---
+++ Intern/Ex7/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7. Показать числа от -N до N
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Show(x);
+
+void Show(int x)
+{
+    for (int i = -x; i <= x; i++)
+    {
+        Console.Write("{0}, ", string.Join(", ", i));
+    }
+}
