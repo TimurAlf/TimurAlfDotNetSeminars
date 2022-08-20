570aafc12b42698c45027e438b6700f5777d7d4c
Timur <47ta@bk.ru>
2022-08-20 20:22:13 +0500

    Inital2

--- Intern/Ex4/Program.cs
+++ Intern/Ex4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Найти максимальное из трех чисел
+
+Console.Write("Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+MaxNum(a, b, c);
+
+void MaxNum(int a, int b, int c)
+{
+    if (a > b && a > c) System.Console.WriteLine($"{a} максимальное число");
+    if (a < b && b > c) System.Console.WriteLine($"{b} максимальное число");
+    if (c > a && c > b) System.Console.WriteLine($"{c} максимальное число");
+}
