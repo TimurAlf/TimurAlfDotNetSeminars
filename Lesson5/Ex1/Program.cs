d2845da44a612fd832e011683b4828647af98ec7
Timur <47ta@bk.ru>
2022-08-22 14:59:18 +0500

    New

---
+++ Lesson5/Ex1/Program.cs
@@ -0,0 +1,51 @@
+﻿// палиндром
+void Log(object o)
+{
+  File.AppendAllText("log.csv", $"{DateTime.Now};{o}\n");
+}
+
+int GetValue(string text)
+{
+  Log("вызван метод GetValue");
+
+  int n = 0;
+
+  while (!(n >= 10000 && n <= 99999))
+  {
+    Console.Write(text);
+    string data = Console.ReadLine();
+    Log(data);
+    n = Convert.ToInt32(data);
+  }
+  Log("метод GetValue окончил работу");
+  return n;
+}
+
+bool IsPolindrom(int value)
+{
+  Log("вызван метод IsPolindrom");
+
+  int t = 0;
+  int tValue = value;
+  while (value != 0)
+  {
+    t = t * 10 + value % 10;
+    value /= 10;
+  }
+  bool result = tValue == t;
+  Log(result);
+  Log("метод IsPolindrom окончил работу");
+
+  return result;
+}
+
+void Main()
+{
+  Log("вызван метод main");
+  int k = GetValue("Введите число из пяти цифр: ");
+  Console.WriteLine($"k: {k}");
+  Console.WriteLine($"IsPolindrom({k}): {IsPolindrom(k)}");
+  Log("метод main окончил работу");
+}
+
+Main();
