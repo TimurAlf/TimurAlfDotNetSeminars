0482126aad8aa37de226c3ee49a92e29bb06cda2
Timur <47ta@bk.ru>
2022-08-26 00:12:19 +0500

    New

---
+++ Intern/Ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿//  9. Показать последнюю цифру трёхзначного числа
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int num = EnterNum();
+if (num > 100 && num < 1000)
+{
+    int lastNum = num % 10;
+    System.Console.WriteLine($"{lastNum}");
+}
+else
+{
+    int num2 = EnterNum();
+}
