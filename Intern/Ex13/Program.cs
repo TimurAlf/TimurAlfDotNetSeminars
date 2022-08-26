0a48c6d444ba107531bf28b710b0ce02bdffe1df
Timur <47ta@bk.ru>
2022-08-26 17:50:03 +0500

    New

---
+++ Intern/Ex13/Program.cs
@@ -0,0 +1,29 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int EnterNum2()
+{
+    System.Console.WriteLine("Введите второе число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int num = EnterNum();
+int num2 = EnterNum2();
+
+if (num % num2 != 0)
+{
+int res = num % num2;
+System.Console.WriteLine($"Число {num} не кратно {num2}, {num} % {num2} = {res} ");
+}
+else 
+{
+    int result = num /num2;
+    System.Console.WriteLine($"Число {num} кратно {num2}, {num} / {num2} = {result} ");
+}
