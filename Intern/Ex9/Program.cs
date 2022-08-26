86d94a3dd723fd6fe21846ff70fa7a5d9acc2f07
Timur <47ta@bk.ru>
2022-08-26 17:24:38 +0500

    New

--- Intern/Ex9/Program.cs
+++ Intern/Ex9/Program.cs
@@ -4,16 +4,15 @@ int EnterNum()
 {
     System.Console.WriteLine("Введите трехзначное число: ");
     int num = Convert.ToInt32(Console.ReadLine());
+    while (num < 100 || num > 1000)
+    {
+        System.Console.WriteLine("Введите корректное число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
     return num;
 }
 
 int num = EnterNum();
-if (num > 100 && num < 1000)
-{
-    int lastNum = num % 10;
-    System.Console.WriteLine($"{lastNum}");
-}
-else
-{
-    int num2 = EnterNum();
-}
+int lastNum = num % 10;
+System.Console.WriteLine($"{lastNum}");
+
