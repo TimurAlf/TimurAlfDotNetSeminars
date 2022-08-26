962330cb2d92a60b077237e8353aee7becf8a931
Timur <47ta@bk.ru>
2022-08-26 18:05:10 +0500

    New

---
+++ Intern/Ex14/Program.cs
@@ -0,0 +1,23 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+if (num > 100 && num < 1000)
+{
+    int thirdNum = num % 10;
+    System.Console.WriteLine($"Третья цифра числа  = {thirdNum}");
+}
+else if (num > 1000)
+{
+    int thirdNum = num / 10 % 10;
+    System.Console.WriteLine($"Третья цифра числа  = {thirdNum}");
+}
+else System.Console.WriteLine("Третьей цифры нет");
+
+
