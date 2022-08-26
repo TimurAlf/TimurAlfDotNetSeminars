ac36773861956dab1e691a59abf24ae5f7bbac62
Timur <47ta@bk.ru>
2022-08-26 17:29:14 +0500

    New

---
+++ Intern/Ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    while (num < 100 || num > 1000)
+    {
+        System.Console.WriteLine("Введите корректное число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int num = EnterNum();
+int secondNum = num /10 % 10;
+System.Console.WriteLine($"{secondNum}");
