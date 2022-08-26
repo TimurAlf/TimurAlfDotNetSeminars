c6dee672e37c64f3db8a5f147b845cacb7fa3466
Timur <47ta@bk.ru>
2022-08-26 17:39:53 +0500

    New

---
+++ Intern/Ex12/Program.cs
@@ -0,0 +1,18 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    while (num < 100 || num > 1000)
+    {
+        System.Console.WriteLine("Введите корректное число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int num = EnterNum();
+int thirdNum = num % 10;
+int resNum = num /100;
+System.Console.WriteLine($"{resNum}{thirdNum}");
