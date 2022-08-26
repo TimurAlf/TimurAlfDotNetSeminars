c0deadf9170edd1eafed22e306efcd2292295e63
Timur <47ta@bk.ru>
2022-08-26 17:34:22 +0500

    New

---
+++ Intern/Ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int EnterNum()
+{
+    System.Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    while (num <= 10|| num > 100)
+    {
+        System.Console.WriteLine("Введите корректное число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int num = EnterNum();
+int firstNum = num / 10;
+int secondNum = num % 10;
+
+if(firstNum>secondNum) System.Console.WriteLine($"{firstNum}");
+else System.Console.WriteLine($"{secondNum}");
